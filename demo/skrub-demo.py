35e9e77d82641f596665359597bd2e028d425ad8
Jerome Dockes <jerome@dockes.org>
2024-09-24 12:17:12 +0200

    iter

--- demo/skrub-demo.py
+++ demo/skrub-demo.py
@@ -1,8 +1,8 @@
-# # Demo time!
-
+# %% [markdown]
 # ## `TableReport`
 
-# +
+
+# %%
 from skrub.datasets import fetch_employee_salaries
 
 dataset = fetch_employee_salaries()
@@ -10,39 +10,40 @@ employees_df, salaries = dataset.X, dataset.y
 employees_df
 
 
-# +
+# %%
 from skrub import TableReport
 
 TableReport(employees_df)
-# -
 
-
+# %% [markdown]
 # ## `tabular_learner`
 
-# +
+
+# %%
 from sklearn.model_selection import cross_val_score
 from skrub import tabular_learner
 
 cross_val_score(tabular_learner('regressor'), employees_df, salaries)
-# -
 
+# %%
 tabular_learner('regressor')
 
-# +
+
+# %%
 from sklearn.linear_model import RidgeCV
 
 tabular_learner(RidgeCV())
-# -
 
+# %% [markdown]
 # ## `TableVectorizer`
 
-# +
+
+# %%
 from skrub import TableVectorizer
 
 vectorizer = TableVectorizer()
 vectorized_employees = vectorizer.fit_transform(employees_df)
 vectorized_employees.dtypes
-# -
 
 vectorizer.kind_to_columns_
 
@@ -53,11 +54,11 @@ vectorizer.all_processing_steps_["date_first_hired"]
 vectorizer.all_processing_steps_["department"]
 
 
-# ---
 
+# %% [markdown]
 # ## Joiners
 
-# +
+# %%
 import polars as pl
 from skrub.datasets import fetch_figshare
 
@@ -65,15 +66,15 @@ airports = pl.from_pandas(
     fetch_figshare("41710257").X
 )
 TableReport(airports)
-# -
 
+# %%
 cols = ["ID", "LATITUDE", "LONGITUDE", "ELEVATION", "STATE"]
 stations = pl.from_dataframe(
     fetch_figshare("41710524").X[cols]
 )
 TableReport(stations)
 
-# +
+# %%
 from skrub import Joiner
 
 joiner = Joiner(
@@ -84,11 +85,13 @@ joiner = Joiner(
 )
 airports_stations = joiner.fit_transform(airports)
 TableReport(airports_stations.sort("skrub_Joiner_distance"))
-# -
 
-# ![](../worst_distance_1.png)
+# %% [markdown]
 
-# +
+# ![](./worst_distance_1.png)
+
+
+# %%
 from skrub import Joiner
 
 joiner = Joiner(
@@ -103,16 +106,21 @@ airports_stations = (
     .sort("skrub_Joiner_distance")
 )
 TableReport(airports_stations)
-# -
 
-# ![](../worst_distance_001.png)
+# %% [markdown]
+
+# ![](./worst_distance_001.png)
+
+# %%
 
 weather = pl.from_pandas(
     fetch_figshare("41771457").X
 )
 TableReport(weather)
 
-# +
+
+# %%
+
 from skrub import AggJoiner
 
 agg_joiner = AggJoiner(
@@ -126,7 +134,9 @@ airports_stations_weather = agg_joiner.fit_transform(airports_stations)
 
 TableReport(airports_stations_weather)
 
-# +
+
+# %%
+
 from sklearn.pipeline import make_pipeline
 
 airports_stations_weather = make_pipeline(
@@ -135,6 +145,3 @@ airports_stations_weather = make_pipeline(
 ).fit_transform(airports)
 
 airports_stations_weather.shape
-# -
-
-
