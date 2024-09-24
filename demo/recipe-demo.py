f4a5ed79f44b56bcfb35744790fcaf93b646d004
Jerome Dockes <jerome@dockes.org>
2024-09-24 13:47:02 +0200

    _

--- demo/recipe-demo.py
+++ demo/recipe-demo.py
@@ -17,6 +17,9 @@ df["salary"] = dataset.y
 
 # %%
 recipe = Recipe(df, y_cols="salary", n_jobs=8)
+recipe
+
+# %%
 recipe.sample()
 
 # %%
@@ -25,28 +28,49 @@ recipe.get_report()
 # %% [markdown]
 # ## Adding transformations to specific columns
 
-# %%
-from skrub import selectors as s
-from skrub import (
-    MinHashEncoder,
-    ToCategorical,
-    DatetimeEncoder,
-    ToDatetime,
-)
+# %% [markdown]
 
-recipe = (
-    recipe.add(ToDatetime(), cols="date_first_hired")
-    .add(DatetimeEncoder(), cols=s.any_date())
-    .add(ToCategorical(), cols=s.string() & s.cardinality_below(30))
-)
+# Don't do this:
+
+# %%
+# # df["date_first_hired"] = pd.to_datetime(df["date_first_hired"])
+
+
+# %%
+from skrub import ToDatetime
+
+recipe = recipe.add(ToDatetime(), cols="date_first_hired")
 recipe
+
+
+# %%
+recipe.get_report()
+
+# %%
+from skrub import DatetimeEncoder
+from skrub import selectors as s
+
+recipe = recipe.add(DatetimeEncoder(), cols=s.any_date())
+recipe
+
+
+# %%
+recipe.get_report()
+
+# %%
+from skrub import ToCategorical
+
+recipe = recipe.add(ToCategorical(), cols=s.string() & s.cardinality_below(30))
+recipe
+
 
 # %% [markdown]
 # ## Specifying alternative transformers and a hyperparameter grid
 
 # %%
 from sklearn.preprocessing import TargetEncoder
-from skrub import choose_from
+
+from skrub import MinHashEncoder, choose_from
 
 recipe = recipe.add(
     choose_from(
@@ -71,14 +95,20 @@ recipe = recipe.add(
 )
 recipe
 
-
 # %% [markdown]
 # ## Getting the model & inspecting search results
 
 # %%
-randomized_search = recipe.get_randomized_search(n_iter=10, cv=3, verbose=1)
+from sklearn.metrics import r2_score
+
+randomized_search = recipe.get_randomized_search(n_iter=16, cv=3, verbose=1)
 randomized_search.fit(recipe.get_x_train(), recipe.get_y_train())
 
+predictions = randomized_search.predict(recipe.get_x_test())
+score = r2_score(recipe.get_y_test(), predictions)
+print(f"R² score: {score:.2f}")
+
+# %%
 recipe.get_cv_results_table(randomized_search)
 
 # %%
